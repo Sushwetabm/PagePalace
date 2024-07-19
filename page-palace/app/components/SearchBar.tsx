18afff8da9fab468d4bd087569f7c8c1824346fa
Sushweta Bhattacharya <Sushwetabm@gmail.com>
2024-07-19 08:21:23 +0530

    updated search bar

--- page-palace/app/components/SearchBar.tsx
+++ page-palace/app/components/SearchBar.tsx
@@ -73,7 +73,7 @@ const SearchBar: React.FC<SearchBarProps> = ({ placeholder }) => {
         <input
           type="text"
           placeholder={placeholder}
-          className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 text-black focus:ring-blue-500"
         />
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md">
           Filter
